a40a376f1acc989638b389af19722d55f3c90f3d
Samuel Park <87939521+p-acid@users.noreply.github.com>
2025-02-17 11:20:39 +0900

    docs: chrome extension basic (#13)

--- src/pages/post-detail/ui/toc.tsx
+++ src/pages/post-detail/ui/toc.tsx
@@ -8,19 +8,19 @@ interface TocProps {
 
 export const Toc = ({ toc }: TocProps) => {
   return (
-    <aside className="absolute left-[calc(100%_+_32px)] h-full w-full max-xl:hidden">
-      <ul className="sticky top-40 my-0 w-full max-w-72 border-l border-slate-700 pl-5">
+    <aside className="absolute left-[calc(100%_+_32px)] h-full w-full max-w-72 max-xl:hidden">
+      <ul className="sticky top-40 my-0 w-full border-l border-slate-700 pl-5">
         {toc.map(({ id, text, depth }, index) => {
           return (
             <li
               key={`${id}-${index}`}
-              className={cn("my-1 list-none pl-0", {
+              className={cn("my-0 list-none pl-0", {
                 [DEPTH_SPACING[depth]]: !!DEPTH_SPACING[depth],
               })}
             >
               <Link
                 href={`#${id}`}
-                className="font-norma break-keep text-sm text-zinc-100 no-underline hover:underline"
+                className="break-keep text-sm font-normal text-zinc-100 no-underline hover:underline"
               >
                 {text}
               </Link>
