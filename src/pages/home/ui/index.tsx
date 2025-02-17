d381aadd674e90254f3e9528f0a8364be74225d5
p-acid <qkrtkstjd01@gmail.com>
2025-02-17 16:13:25 +0900

    style: remove lacquer font

--- src/pages/home/ui/index.tsx
+++ src/pages/home/ui/index.tsx
@@ -1,17 +1,9 @@
-import { Lacquer } from "next/font/google";
-
 import { contentsBases } from "@/shared/constants/paths";
-import { resumeUrl } from "@/shared/constants/url";
 import { PostFrontmatter } from "@/shared/types/contents";
-import { cn } from "@/shared/utils/cn";
 import { getContents } from "@/shared/utils/contents";
 import { PostList } from "@/widgets/post-list";
-import Introduce from "./introduce.mdx";
 
-const lacquer = Lacquer({
-  weight: "400",
-  subsets: ["latin"],
-});
+import Introduce from "./introduce.mdx";
 
 export const HomePage = async () => {
   const posts = getContents<PostFrontmatter>({ base: contentsBases.posts });
@@ -19,23 +11,6 @@ export const HomePage = async () => {
 
   return (
     <section className="space-y-8">
-      <h2
-        className={cn(
-          lacquer.className,
-          "flex items-center gap-1.5 text-xl text-white",
-        )}
-      >
-        welcome to
-        <a
-          target="_blank"
-          href={resumeUrl}
-          className="text-slate-400 transition-colors duration-500 hover:text-green-500"
-        >
-          351
-        </a>
-        blog
-      </h2>
-
       <article className="prose prose-invert">
         <Introduce />
       </article>
--- src/widgets/layouts/navigation.tsx
+++ src/widgets/layouts/navigation.tsx
@@ -1,30 +1,24 @@
 "use client";
 
-import { Lacquer } from "next/font/google";
 import Link from "next/link";
 
 import { navigationRoutes } from "@/shared/constants/page-routes";
 import { cn } from "@/shared/utils/cn";
 import { usePathname } from "next/navigation";
 
-const lacquer = Lacquer({
-  weight: "400",
-  subsets: ["latin"],
-});
-
 export const Navigation = () => {
   const pathname = usePathname();
 
   return (
-    <nav className={cn(lacquer.className, "mb-10 flex items-center gap-8")}>
+    <nav className={cn("mb-10 flex items-center gap-8")}>
       {navigationRoutes.map(({ text, path, matcher }) => (
         <Link
           key={path}
           href={path}
           className={cn(
-            "text-lg text-slate-400 transition-colors duration-300 hover:text-slate-100",
+            "text-base font-semibold text-slate-400 transition-colors duration-300 hover:text-slate-100 hover:underline",
             {
-              "text-slate-100": matcher(pathname),
+              "text-slate-100 underline": matcher(pathname),
             },
           )}
         >
